3b45dad677b93e57101fdf4ab845bc40c873f4cb
Alex763rus <163avito16394@mail.ru>
2019-04-26 10:43:21 +0400

    почистил лишнее

--- KrokHomeWork12/src/main/java/student/Student.java
+++ KrokHomeWork12/src/main/java/student/Student.java
@@ -5,19 +5,12 @@
  */
 package student;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import exception.ReadWriteEx;
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileReader;
-import java.io.FileWriter;
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.logging.Level;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import reflectionapi.Autoincrement;
